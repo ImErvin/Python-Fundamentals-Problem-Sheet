856157e17a080dbfc7d1ad48a03e058a96d86cd3
Ervin Mamutov <employervin@gmail.com>
2017-09-19 15:19:24 +0100

    Problem2 solution added

---
+++ problem2.py
@@ -0,0 +1,5 @@
+import time # Imported time library for problem 2
+
+# Use the time library's string format time to present a formatted time and date
+# Use print() to print it to the console
+print("Current date and time %s\n"  % time.strftime("%c") )
