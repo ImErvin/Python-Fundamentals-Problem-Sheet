62ff47a18392ba657360978130ed65545e7ac781
Ervin Mamutov <employervin@gmail.com>
2017-09-20 11:39:26 +0100

    Added comments for problem 7

--- problem7.py
+++ problem7.py
@@ -4,10 +4,13 @@ def palindromeCheck(someString):
     nonReversedString = [i for i in someString]
     # Local variable reversedString is a list of characters that make up the string "someString"  
     reversedString = [someString[i] for i in range(len(nonReversedString)-1, -1, -1)]
+    # If the two lists above equal, then it's a palindrome
     if(nonReversedString == reversedString):
         print(someString,"is a palindrome!")
+    # Or else it's not a palindrome
     else:
         print(someString,"is not a palindrome!")
 
+# Ask the user for an input and pass that input into the function to check if it's a palindrome
 palindrome = input("Type in a word to check if it's a palindrome: ")
 palindromeCheck(palindrome)
